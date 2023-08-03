8762e5b72244a992723ec320afa2765a708b1340
yemidada <yemidada@gmail.com>
2023-08-03 13:02:26 +0100

    Fix linter error

--- src/__tests__/NavBar.test.js
+++ src/__tests__/NavBar.test.js
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import NavBar from '../components/NavBar';
